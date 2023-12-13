c753719363b4c10efdc4af9817e2fd4f81e056d5
Ravichandran T S <ravichandrants202@gmail.com>
2023-12-13 10:17:51 +0530

    Debuged some template setting

--- schoolproject/schoolproject/settings.py
+++ schoolproject/schoolproject/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
+STATICFILE_DIRS = [
     STATIC_DIR
 ]
 # Default primary key field type
