570a304dcdbcff05026690c7c5d39b34d4e98ab3
Ravichandran T S <ravichandrants202@gmail.com>
2023-12-13 12:44:54 +0530

    Added user-creation-form

--- schoolproject/schoolapp/urls.py
+++ schoolproject/schoolapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("signin",views.signin,name="signin")
+    path("",views.home,name="home"),
+    path("signin",views.signin,name="signin"),
+    path("create-user",views.create_new_user,name="createuser")
 ]
--- schoolproject/schoolapp/views.py
+++ schoolproject/schoolapp/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
-def signin(requests):
-    return render(requests,"signin.html")
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+            return redirect("signin")
+
+    return render(request, "signin.html")
+
+
+def home(request):
+    return render(request,"home.html")
+
+def create_new_user(request):
+    return render(request,"create-user-form.html")
